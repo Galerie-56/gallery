e3a013d3f3dda83275ac9b86950145fdab351b03
Alexandra Spalato <alexaspalato@gmail.com>
2024-07-09 15:20:41 +0200

    publications

--- app/components/bloks/Publications.tsx
+++ app/components/bloks/Publications.tsx
@@ -1,5 +1,5 @@
-import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
-import { PublicationStoryblok, PublicationsStoryblok } from "~/types";
+import { storyblokEditable, StoryblokComponent } from '@storyblok/react';
+import { PublicationStoryblok, PublicationsStoryblok } from '~/types';
 
 export const Publications = ({ blok }: PublicationsStoryblok) => {
   const { items, _uid, book_cover, purchase_link } = blok;
@@ -7,14 +7,14 @@ export const Publications = ({ blok }: PublicationsStoryblok) => {
     <div
       {...storyblokEditable(blok)}
       key={_uid}
-      className="flex flex-col md:flex-row justify-between items-start py-4 gap-10 md:gap-20"
+      // className="flex flex-col md:flex-row justify-between items-start py-4 gap-10 md:gap-20"
     >
       <div>
         {items.map((nestedBlok: PublicationStoryblok) => (
           <StoryblokComponent key={nestedBlok._uid} blok={nestedBlok} />
         ))}
       </div>
-      <div>
+      {/* <div>
         <a href={purchase_link} target="_blank" rel="noopener noreferrer">
           <img src={`${book_cover.filename}/m/352x0/`} alt={book_cover.alt} />
         </a>
@@ -29,7 +29,7 @@ export const Publications = ({ blok }: PublicationsStoryblok) => {
             </a>
           </button>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
