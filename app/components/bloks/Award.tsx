8b4e74918e4d1be3b5fa3da3dbd87a8f4d04df03
Alexandra Spalato <alexaspalato@gmail.com>
2024-07-30 15:58:12 +0200

    award image

--- app/components/bloks/Award.tsx
+++ app/components/bloks/Award.tsx
@@ -14,7 +14,7 @@ export const Award = ({ blok }: AwardStoryblok) => {
       <div className="mb-2 w-full">
         <a href={link?.cached_url} target="_blank" rel="noopener noreferrer">
           <img
-            src={`${thumbnail.filename}/m/800x500/`}
+            src={`${thumbnail.filename}/m/1600x1000/`}
             alt={thumbnail.alt}
             className="w-full h-auto"
           />
